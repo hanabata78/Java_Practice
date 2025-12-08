c46b03b2911e189edd13ad700edefce7e81af063
hanabata78 <hanahana221@outlook.jp>
2025-12-08 15:48:41 +0900

    20251208

---
+++ src/day20251208/Main.java
@@ -0,0 +1,15 @@
+package day20251208;
+
+/*オブジェクト指向について学ぶ
+ *まず、実行用クラス(Mainメソッド）to
+ *設計図クラス（属性と操作）を記述
+ *設計図クラスだと変数の定義、
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		aaaa
+
+	}
+
+}
